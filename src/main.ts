f4c1c769df288e6e8a306f3df7453599d6657435
Camopoltergeist <camopoltergeist@outlook.com>
2022-09-21 22:18:39 +0300

    Created basic three program

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,42 @@
+import { OrthographicCamera, Scene, WebGLRenderer } from "three";
 import "./style.css";
 
-console.log("Hello world!");
+const mainCanvas = document.querySelector("#mainCanvas") as HTMLCanvasElement | null;
+
+if(mainCanvas === null){
+	throw new Error("Could not find mainCanvas in document!");
+}
+
+const renderer = new WebGLRenderer({
+	canvas: mainCanvas,
+	antialias: true,
+	preserveDrawingBuffer: true,
+	powerPreference: "high-performance"
+});
+
+const scene = new Scene();
+const camera = new OrthographicCamera();
+
+const resizeObserver = new ResizeObserver((entries, observer) => {
+	for(const entry of entries){
+		const width = entry.devicePixelContentBoxSize[0].inlineSize;
+		const height = entry.devicePixelContentBoxSize[0].blockSize;
+
+		renderer.setSize(width, height, false);
+
+		camera.right = width;
+		camera.bottom = height;
+
+	}
+});
+
+resizeObserver.observe(mainCanvas);
+
+function step(time: DOMHighResTimeStamp){
+	requestAnimationFrame(step);
+
+	renderer.clear();
+	renderer.render(scene, camera);
+}
+
+requestAnimationFrame(step);
