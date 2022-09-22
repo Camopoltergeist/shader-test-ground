0c4a3b5fb972983b01bb0163f4de9c2868816dee
Camopoltergeist <camopoltergeist@outlook.com>
2022-09-22 19:40:28 +0300

    Cool bouncing circle

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -7,5 +7,16 @@ uniform float time;
 uniform vec2 screenSize;
 
 void main(){
-	gl_FragColor = vec4(v_UV, 0, 1);
+	vec2 posDiff = v_ScreenPos - (screenSize / 2.f);
+
+	vec2 powPos = pow(posDiff, vec2(2));
+	float dist = sqrt(powPos.x + powPos.y);
+
+	dist /= 200.f + sin(time / 100.f) * 10.f;
+
+	dist = step(dist, 1.f);
+
+	vec3 color = vec3(0, 0.5, 1);
+
+	gl_FragColor = vec4(color * dist, 1);
 }
