d2629c183b6bf1ebf99ae2b060bbf5c02262e25a
Camopoltergeist <camopoltergeist@outlook.com>
2022-09-28 12:08:52 +0300

    Added glow effect

--- src/fragment.glsl
+++ src/fragment.glsl
@@ -9,7 +9,7 @@ uniform float time;
 uniform vec2 screenSize;
 uniform sampler2D fftTex;
 
-const float radius = 200.0;
+const float radius = 400.0;
 const float fftRange = 1.0 / 3.1;
 
 float angle(vec2 a, vec2 b){
@@ -20,20 +20,26 @@ float angle(vec2 a, vec2 b){
 void main(){
 	vec2 screenCenter = screenSize / 2.0;
 	float dist = distance(v_ScreenPos, screenCenter);
+	dist /= radius;
 
-	float angel = angle(screenCenter, v_ScreenPos);
-	float sampleT = (angel / 2.0) / PI;
+	// float angel = angle(screenCenter, v_ScreenPos);
+	float sampleT = dist;
 	float fftU = mix(0.0, fftRange, sampleT);
 
 	float amp = texture2D(fftTex, vec2(fftU, 0.0)).r;
 
-	dist /= radius;
 	dist -= amp;
 	dist = 1.0 - dist;
+	dist *= amp;
 
 	// float stemp = step(dist, 1.0);
 
-	vec3 color = vec3(0, 0.5, 1);
+	vec3 color = vec3(1, 0.5, 1);
+	vec3 altColor = vec3(1, 1, 1);
 
-	gl_FragColor = vec4(color * dist, 1);
+	float altMod = max(0.0, dist - 1.0);
+
+	altColor *= altMod;
+
+	gl_FragColor = vec4(color * dist + altColor, 1);
 }
